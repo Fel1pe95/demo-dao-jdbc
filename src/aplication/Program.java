7dd4e7cc797a841b532ec7801c3fc2b0dab4a502
Luis felipe <84816784+Fel1pe95@users.noreply.github.com>
2021-05-28 19:14:37 -0300

    SellerDaoJDBC and DaoFactory

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,6 +2,8 @@ package aplication;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,7 @@ public class Program {
 		Department obj = new Department(1, "books");
 		Seller seller = new Seller("luis", "@gmail.com", new Date(), 3000.0, 1, obj);
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 
 	}
