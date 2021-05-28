41d6c8554381038edc0b09596c53dd6ee9576933
Luis felipe <84816784+Fel1pe95@users.noreply.github.com>
2021-05-28 18:49:32 -0300

    Seller entity class

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,12 +1,17 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "books");
-		System.out.println(obj);
+		Seller seller = new Seller("luis", "@gmail.com", new Date(), 3000.0, 1, obj);
+		
+		System.out.println(seller);
 
 	}
 
